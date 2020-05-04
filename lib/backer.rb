fec2a5ea54acf0978ec377b9056d0349d1115b06
Saida Davis <saihdavis@gmail.com>
2020-05-04 17:51:52 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,8 +10,8 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
-    project.backer = self 
-    project.backers << self
+    #project.backer = self 
+    #project.backers << self
   end
   
   
